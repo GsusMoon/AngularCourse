aca62d9229044d30cfb60a86af4a1c228f1212c8
gsusmoon <gsusecheve@hotmail.com>
2016-08-31 11:59:52 -0600

    Ng-src, ng-click y tabs

--- www/js/app.js
+++ www/js/app.js
@@ -3,11 +3,41 @@
 	var gem = { 
 		name: 'Azurite', 
 		price: 2.95,
-		description:'Esta Di-Vi-Na'
+		description:'Esta Di-Vi-Na',
+		canPurchase:false,
+		soldOut:true
 	};
+
+	var gems =[ 
+		{name: 'Azurite', 
+		price: 2.95,
+		images: [
+		{id: 0, url:'img/gem-01.gif'},
+		{id: 1, url:'img/gem-02.gif'},
+		{id: 2, url:'img/gem-03.gif'}
+	]
+	},
+	{name: 'Bloodstone', 
+		price: 5.95,
+		images: [
+		{id: 0, url:'img/gem-01.gif'},
+		{id: 1, url:'img/gem-02.gif'},
+		{id: 2, url:'img/gem-03.gif'}
+	]
+	},
+	{name: 'Zircon', 
+		price: 3.95,
+		images: [
+		{id: 0, url:'img/gem-01.gif'},
+		{id: 1, url:'img/gem-02.gif'},
+		{id: 2, url:'img/gem-03.gif'}
+	]
+	},
+	];
+
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController',function(){
-		this.product = gem;
+		this.products = gems;
 	});
 })();
